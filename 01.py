a117fee461a4cfdb2500eccdd6bd7d423b4f28db
AidanaTokeken <aidanatokeken@gmail.com>
2026-04-18 15:00:05 +0500

    Add Practice7 - Pygame games: Mickey's clock, music player, moving ball

---
+++ 01.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init() # initializes all the pygame sub-modules
+
+screen = pygame.display.set_mode((800, 480)) # creating a game window
+# set_mode() takes a tuple as an argument
+
+while True: # game loop
+    pass    # use ctrl+c in the terminal to stop it
